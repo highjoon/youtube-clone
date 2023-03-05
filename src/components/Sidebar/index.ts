3ac3c7dbf61b9eff99e317cfd369f089cd5b2626
highjoon <yundosa2@gmail.com>
2023-03-06 03:17:36 +0900

    test: sidebar

---
+++ src/components/Sidebar/index.ts
@@ -0,0 +1,8 @@
+import { Container, MenuIcon, Title } from './Sidebar';
+
+const Sidebar = Object.assign(Container, {
+  Title,
+  Icon: MenuIcon,
+});
+
+export default Sidebar;
--- src/components/Sidebar/tests/Sidebar.test.tsx
+++ src/components/Sidebar/tests/Sidebar.test.tsx
@@ -1,9 +1,38 @@
-import { render } from '@testing-library/react';
+import { matchers } from '@emotion/jest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-import Sidebar from '../Sidebar';
+import Sidebar from '../';
+
+expect.extend(matchers);
 
 describe('Sidebar', () => {
+  const SidebarComponent = (
+    <Sidebar>
+      <Sidebar.Icon icon={'Home'} />
+      <Sidebar.Title>Home</Sidebar.Title>
+    </Sidebar>
+  );
+
   it('정상적으로 렌더링됩니다.', () => {
-    render(<Sidebar icon="Home" />);
+    const { asFragment } = render(SidebarComponent);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('마우스 hover 시 Container 색상이 var(--color-black-dark4)로 변합니다.', async () => {
+    const user = userEvent.setup();
+
+    render(SidebarComponent);
+
+    const component = screen.getByTestId('container');
+
+    user.hover(component);
+    expect(component).toHaveStyleRule('background', 'var(--color-black-dark4)', {
+      target: ':hover',
+    });
+
+    user.unhover(component);
+    expect(component).not.toHaveStyleRule('background', '');
   });
 });
