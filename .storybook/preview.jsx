d349fdf8a14bec49db5192e94c2c0171129cf14f
highjoon <yundosa2@gmail.com>
2023-03-07 00:35:19 +0900

    design: set emotion globalStyles in storybook

---
+++ .storybook/preview.jsx
@@ -0,0 +1,22 @@
+import { Global } from '@emotion/react';
+
+import GlobalStyles from '@/styles/globalStyles';
+
+export const decorators = [
+  Story => (
+    <>
+      <Global styles={GlobalStyles} />
+      <Story />
+    </>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
